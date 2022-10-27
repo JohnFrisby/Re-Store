50d61c5e0c849dc355cc38eb2abd8b7e60975026
Denys Yevchenko <31778102+JohnFrisby@users.noreply.github.com>
2022-10-27 21:24:11 +0300

    make spinner component

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,7 +1,14 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import './app.css';
+import Spinner from "../spinner";
 
 export default class App extends Component {
+
+    render() {
+        return (
+            <Spinner />
+        )
+    }
     
 }
---
+++ src/components/spinner/spinner.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+import './spinner.css';
+
+const Spinner = () => {
+    return (<div class="lds-roller">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+        </div>);
+}
+export default Spinner;
