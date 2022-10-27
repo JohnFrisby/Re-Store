54c079040bad1d11616f2a2a8000105b98bcb04a
Denys Yevchenko <31778102+JohnFrisby@users.noreply.github.com>
2022-10-27 20:54:11 +0300

    make app files

---
+++ src/components/app/app.js
@@ -0,0 +1,7 @@
+import React, { Component } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import './app.css';
+
+export default class App extends Component {
+    
+}
